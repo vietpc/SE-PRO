9b107d3884a79b507d71f9cc4c0579036b798b5f
nguyenluongbac <nguyenluongbac301092@gmail.com>
2013-04-12 03:33:22 +0700

    tao ra form lay lai password

---
+++ forgetpass.php
@@ -0,0 +1,39 @@
+
+<table width="600" height="200" border="0" align="center" style ="margin-top: 100; margin-bottom: 0" cellpadding="50" cellspacing="0" bgcolor="#CCCCCC">
+<tr>
+<form name ="form1" method="post" action="checkpass.php">
+<tr>
+<td><strong>
+<html>
+<body >
+<p align ="center" style ="margin-left: 10; margin-right: 5; margin-top: 10; margin-bottom: 1">FIND YOUR ACCOUNT</p>
+</body>
+</html>
+ </strong></td>
+</tr>
+
+<td>
+<table width="60%" border ="0" align="center" style ="margin-top:0; margin-bottom: 30 "cellpadding="4" cellspacing ="4" bgcolor ="#FFFFFF">
+<tr>
+<td width ="70" align="center" colspan="5"><strong>Enter your Username and Email</strong></td>
+</tr>
+
+<tr>
+<td width ="70" align="center">Username</td>
+<td width = "">:</td>
+<td width ="294" align="center" ><input name= "myusername" type="text" id="myusername"></td>
+</tr>
+<tr>
+<td width ="70" align="center">Email</td>
+<td width = "">:</td>
+<td width ="294" align="center" ><input name= "myemail" type="text" id="myemail"></td>
+</tr>
+
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type ="submit" name="Enter" value ="Enter"></td>
+</tr>
+</table>
+</form>
+</table>

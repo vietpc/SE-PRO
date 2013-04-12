f86885104f1f6b61ed77ba6c44e967ae3f4c3f75
truong-github <lyhoang2001@gmail.com>
2013-04-12 01:32:23 -0700

    form them mon hoc

---
+++ formaddsubject.php
@@ -0,0 +1,31 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<form name ="form1" method="post" action="addsubject.php">
+<table width="60%" border ="0" align="center" cellpadding="4" cellspacing ="4" bgcolor ="#FFFFFF">
+<tr>
+<td colspan="5" align="center"><strong>Thêm Môn Học </strong></td>
+</tr>
+<tr>
+<td width ="200" align="center">Mã Môn Học</td>
+<td width = "">:</td>
+<td width ="400"><input name="mamonhoc" type="text" id="mamonhoc"></td>
+</tr>
+<tr>
+<td width ="200" align="center">Tên Môn Học</td>
+<td width = "">:</td>
+<td width ="400" ><input name="tenmonhoc" type="text" id="tenmonhoc"></td>
+</tr>
+<tr>
+<td width ="200" align="center">Số Tín Chỉ</td>
+<td width = "">:</td>
+<td width ="400" ><input name="sotinchi" type="text" id="sotinchi"></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type ="submit" name="Thêm" value ="Thêm"></td>
+</tr>
+<tr> 
+<td  align ="center"><a href='Logout.php'>Logout</a></td>
+ </tr> 
+</table>
+</form>

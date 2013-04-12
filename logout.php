cf317a74a66d8bd2e8678e08c57ef2bb663e17c1
vietpc <vietpc_56@vnu.edu.vn>
2013-04-12 14:23:31 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,49 @@
-<?php
-session_start();
-if (isset($_SESSION['myname'])) {
-  unset($_SESSION['myname']); // Hủy sessionk
-}
+<html>
+<head>
+  <title>Làm quen với php</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+</head>
+	<style>
+	body {background: url(bg_2cwebvn.png);color:#00FF00; font-size:16px;}
+	h2{margin:50px 0px 30px;}
+	</style>
+<body>
+		<font face="Bodoni" size="5" color="red">
+		<center><h1>Hiển thị database </h1></center>
+	<center>
 
-// trở về trang chủ
-header('Location: index.php');
+<?php
+	$con = mysql_connect("localhost","root","");
+	if (!$con)
+	{
+		die('không thể kết nối tới database: ' . mysql_error());
+	}
+
+	mysql_select_db("subject",$con) or  die ("không chọn được database");
+	$result = mysql_query("SELECT * FROM subject");
+	if(mysql_num_rows($result)){
+		echo "<table boder = '1' cellpadding = '5px'>
+		<tr> <th>STT</th>	<th>Ma Mon Hoc</th>  <th>Mon Hoc</th><th>So Tin Chi</th></tr>";
+		
+		while($row = mysql_fetch_array($result)){
+			echo "<tr>";
+			echo "<td>".$row['STT']."</td>";
+			echo "<td>".$row['Ma Mon Hoc']."</td>";
+			echo "<td>".$row['Mon Hoc']."</td>";
+			echo "<td>".$row['So Tin Chi']."</td>";
+			echo "<tr>";
+		}
+		echo "</table>";
+		}
+	
+	
 ?>
+
+	
+
+
+
+<p align style ="margin-left: 10; margin-right: 5; margin-top: 20; margin-bottom: 0"><a href='subject.php'><strong>Thêm Môn Học</strong></p>
+</center>
+</body>
+</html>

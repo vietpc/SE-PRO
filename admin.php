2fbd256165995f1f307748c386978c62235cfcc3
nguyenluongbac <nguyenluongbac301092@gmail.com>
2013-04-25 05:46:30 -0700

    Create admin.php
    Admin lua chon cac chuc nang

---
+++ admin.php
@@ -0,0 +1,11 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<html>
+<body>
+  <p align ="center" style ="margin-left: 10; margin-right: 5; margin-top: 20; margin-bottom: 0"><strong>Wellcome ! Chương Trình Quản Lí Đào Tạo</strong></p>
+	<p align ="center" style ="margin-left: 10; margin-right: 5; margin-top: 20; margin-bottom: 0"><strong>Wellcome ! Admin.</strong></p>
+	<p align ="center" style ="margin-left: 10; margin-right: 5; margin-top: 20; margin-bottom: 0"><a href='subject.php'>Các Môn Học Kì Này</p>
+	<p align ="center" style ="margin-left: 10; margin-right: 5; margin-top: 20; margin-bottom: 0"><a href='moreuser.php'>Tạo Người Dùng Mới</p>
+	<p align ="center" style ="margin-left: 10; margin-right: 5; margin-top: 20; margin-bottom: 0"><a href='formdelete.php'>Xóa Tài Khoản</p>
+	<p align ="center" style ="margin-left: 10; margin-right: 5; margin-top: 20; margin-bottom: 0"><a href='Logout.php'>Logout</a></p>
+</body>
+</html>

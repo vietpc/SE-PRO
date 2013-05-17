06c2593e291605afb52d6f20246abe236af6b8f8
truong-github <lyhoang2001@gmail.com>
2013-05-17 11:07:48 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,35 +1,39 @@
-<?php
-  require_once("connect.php");
-	$monhoc = $_POST['monhoc'];
-	$msmh = $_POST['msmh'];
-	$tinchi = $_POST['tinchi'];
-	if( !$monhoc || !$msmh || !$tinchi ) {
-		echo "Xin vui lòng nhập đầy đủ các thông tin. ";
-		echo "<p><a href='javascript:history.go(-1)'>Nhấp vào đây để quay trở lại</a></p>";
-		echo "<p><a href='index.php'>Nhấp vào đây để quay trở lại trang chủ</a></p>";
-	}else {
-		$sql = "SELECT * FROM mon_hoc WHERE ma_mon_hoc = '{$msmh}'";
-		$check = @mysql_query($sql);
-		$result = @mysql_num_rows($check);
-		if( $result > 0 ){
-			echo "Môn học đã tồn tại";
-			echo "<p><a href='javascript:history.go(-1)'>Nhấp vào đây để quay trở lại</a></p>";
-			echo "<p><a href='index.php'>Nhấp vào đây để quay trở lại trang chủ</a></p>";
-			exit;
-		}else{
-			$query ="INSERT INTO mon_hoc (ma_mon_hoc , ten_mon_hoc , so_tin_chi)
-					VALUES ('{$msmh}' , '{$monhoc}' , '{$tinchi}')";
-			$result = @mysql_query($query);
-			if($result){
-				echo "Đã thêm thành công";
-                echo "<p><a href='list_subject.php'>Nhấp vào đây để xem danh sach cac mon hoc</a></p>";
-                echo "<p><a href='index.php'>Nhấp vào đây để quay trở lại trang chu</a></p>";
-			}else{
-				echo "Co loi xay ra trong qua trinh thao tac voi CSDL.";
-				echo "<p><a href='javascript:history.go(-1)'>Nhấp vào đây để quay trở lại</a></p>";
-                echo "<p><a href='index.php'>Nhấp vào đây để quay trở lại trang chu</a></p>";
-				exit;
-			}
-		}
-	}
-?>
+<html>
+	<body>
+		<form action = "check_change_infor.php" method = "post">
+			<table border="0" cellspacing="10" cellpadding="10" style="border-collapse:collapse; width:100%;">
+
+				<tr>                                    
+					<td >
+						<strong>Nhap ma so sinh vien</strong>  
+						<br />
+						<input name="mssv" value="" type="text" />
+					</td>
+				</tr>
+
+				<tr>                                    
+					<td >
+						<strong>Nhập tên mới</strong>  
+						<br />
+						<input name="newname" value="" type="text" />
+					</td>
+				</tr>
+
+				<tr>                                    
+					<td >
+						<strong>Nhập mật khẩu mới</strong>  
+						<br />
+						<input name="newpass" value="" type="text" />
+					</td>
+				</tr>
+								
+				<tr>                                    
+					<td >
+						<input name="change" value="Change" type="submit" />
+					</td>
+				</tr>
+
+			</table>
+		</form>
+	</body>
+</html>

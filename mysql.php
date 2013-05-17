86fb3366f541da8c69879adc6cda578b88a87075
vietpc <vietpc_56@vnu.edu.vn>
2013-05-17 10:44:30 +0700

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -1,9 +1,1 @@
-<?php
-$db_host = "localhost"; // Giữ mặc định là localhost
-$db_name    = 'anything';// Can thay doi
-$db_username    = 'root'; //Can thay doi
-$db_password    = '';//Can thay doi
-@mysql_connect("{$db_host}", "{$db_username}", "{$db_password}") or die("Không thể kết nối database");
-@mysql_select_db("{$db_name}") or die("Không thể chọn database");
-?>
-?>
+ 

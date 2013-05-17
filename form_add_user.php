631d8553ea21011c61807cc41cdadfeda34078bb
truong-github <lyhoang2001@gmail.com>
2013-05-17 14:07:49 +0700

    Update and rename moreuser.php to form_add_user.php

---
+++ form_add_user.php
@@ -0,0 +1,50 @@
+<html>
+	<head>
+		<title>WEBSITE QUAN LY DAO TAO</title>
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<style>
+			body {background: url(bg_2cwebvn.png);color:#00FF00; font-size:16px;}
+			h2{margin:50px 0px 30px;}
+		</style>
+	</head>	
+	<body> 
+		<font face="Bodoni" size="5" color="red">
+		<center><h1> BANG THEM MON HOC</h1></center>
+		<center>
+		<form action="check_user.php" method="post">                    
+			<table align="centre" border="0"  id="table" style="margin-left-"150px"; border-collapse:collapse; width:100%; ">
+
+				<tr>  
+					<td>
+						<strong>Ma so sinh vien</strong><br />
+						<input name="mssv" value="" type="text">
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						<strong>Ho va ten</strong><br />
+						<input name="fullname" value="" type="text">
+					</td>
+				</tr>
+                                
+				<tr>
+					<td>
+						<strong>Mat khau dang nhap</strong><br />
+						<input name="password" value="" type="text">
+					</td>
+				</tr>
+							
+				<tr>
+					<td>
+						<input name="them" value="Add" type="submit">
+					</td>
+                </tr>
+
+			</table>               
+		</form>
+		<p><a href='index.php'>Nhap vao day de quay lai trang chu</a></p>
+		</center>
+	</body>
+</html>
+

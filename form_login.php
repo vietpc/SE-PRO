37a4d9427bf9b591561a983f407e6481e2019252
nguyenluongbac <nguyenluongbac301092@gmail.com>
2013-05-20 02:01:40 -0600

    Create form_login.php

---
+++ form_login.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+		<title>WEBSITE QUẢN LÝ ĐÀO TẠO</title>
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<style>
+			body {background: url(bg_2cwebvn.png);color:#00FF00; font-size:16px;}
+			h2{margin:50px 0px 30px;}
+		</style>
+	</head>	
+	<body>
+		<div id="containt" align="center">
+		<font face="Bodoni" size="5" color="red">
+		<center><h1>TRANG ĐĂNG NHẬP</h1></center>
+		<form name="login" action ="check_login.php" method="post" >
+			<table>
+             
+				<tr>
+					<td>Tên truy nhập:</td>
+					<td> <input type="text" name="username" size="20"></td>
+				</tr>
+             
+				<tr>
+					<td>Mật khẩu:</td>
+					<td><input type="password" name="password" size="20"></td>
+				</tr>
+
+				<tr>
+					<td><input type="submit" name="submit" value="Đăng nhập"></td>
+				</tr>
+
+			</table>
+		</form>
+		</div>
+	</body>
+</html>

b0ce1051ba015df8024ae879d2b225d2dcf25fc5
truong-github <lyhoang2001@gmail.com>
2013-05-25 13:59:29 +0700

    Trang chủ làm việc của admin

--- admin_index.php
+++ admin_index.php
@@ -1,6 +1,6 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <html>
-    <style type='text/css'>
+	<style type='text/css'>
 	body{
 		background: #eee;
 		color: #e00;
@@ -13,19 +13,41 @@
 	}
 	</style>
 	<body>
-		<p align ="center" style ="margin-left: 10; margin-right: 5; margin-top: 20; margin-bottom: 0"><strong>Chương Trình Quản Lí Đào Tạo</strong></p>
-		<p align ="center" style ="margin-left: 10; margin-right: 5; margin-top: 20; margin-bottom: 0"><strong>Wellcome ! Admin.</strong></p></br>
+		<p align ="center" 
+			style ="margin-left: 10; margin-right: 5;
+					margin-top: 20; margin-bottom: 0">	
+			<strong>Chương Trình Quản Lí Đào Tạo</strong>
+		</p>
+		<p align ="center" 
+			style ="margin-left: 10; margin-right: 5; margin-top: 20;
+					margin-bottom: 0">
+					<strong>Wellcome ! Admin.</strong>
+		</p><br/>
 	    <table align=center border=2 width=500>
 		    <tr>
 				<td>
 					<ul>
-						<li><a href='form_add_user.php'>Tạo Tài Khoản Sinh Viên</li>
-						<li><a href='form_delete_user.php'>Xóa Tài Khoản Sinh Viên</li>
-						<li><a href='form_add_subject.php'>Thêm Môn Học</li>
-						<li><a href='form_delete_subject.php'>Xóa Môn Học</li>
-						<li><a href='list_student.php'>Danh Sach Các Sinh Viên</li>
-						<li><a href='list_subject.php'>Các Môn Học Kỳ Này</li>
-						<li><a href='logout.php'>Thoát</li>
+						<li><a href='form_add_user.php'>
+							Tạo Tài Khoản Sinh Viên
+						</li>
+						<li><a href='form_delete_user.php'>
+							Xóa Tài Khoản Sinh Viên
+						</li>
+						<li><a href='form_add_subject.php'>
+							Thêm Môn Học
+						</li>
+						<li><a href='form_delete_subject.php'>
+							Xóa Môn Học
+						</li>
+						<li><a href='list_student.php'>
+							Danh Sach Các Sinh Viên
+						</li>
+						<li><a href='list_subject.php'>
+							Các Môn Học Kỳ Này
+						</li>
+						<li><a href='logout.php'>
+							Thoát
+						</li>
 					</ul>
 				</td>
 			</tr>

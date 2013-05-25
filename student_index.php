d86f2aeb35a3c4b18a53893651a002709f6d9401
truong-github <lyhoang2001@gmail.com>
2013-05-25 14:22:58 +0700

    Trang chủ làm việc của student

---
+++ student_index.php
@@ -0,0 +1,49 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<html>
+	<style type='text/css'>
+	body{
+		background: #eee;
+		color: #e00;
+	}
+	a{
+		color:#00e;
+	}
+	a:hover{
+		color: #ee0;
+	}
+	</style>
+	<body>
+		<br/>
+		<br/>
+		<p><center><h1><strong>
+							Chương Trình Quản Lí Đào Tạo
+						</strong></h1><center/></p>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+	    <table align=center border=2 width=500>
+		    <tr>
+				<td>
+					<ul>
+						<li>
+							<a href='form_register.php'>Đăng Ký Môn Học
+						</li>
+						<li>
+							<a href='tbk.php'>Các Môn Học Đã Đăng Ký
+						</li>
+						<li>
+							<a href='infor.php'>Xem Thông Tin Cá Nhân
+						</li>
+						<li>
+							<a href='form_change_infor.php'>Sửa Thông Tin Cá Nhân
+						</li>
+						<li>
+							<a href='stulogout.php'>Thoát
+						</li>
+					</ul>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>

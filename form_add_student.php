0850f69435422e643441b1a6184cae246ec6ee53
truong-github <lyhoang2001@gmail.com>
2013-05-25 14:19:42 +0700

    Form dùng cho chức năng thêm sinh viên

--- form_add_student.php
+++ form_add_student.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>WEBSITE QUAN LY DAO TAO</title>
+		<title>WEBSITE QUẢN LÝ ĐÀO TẠO</title>
 			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<style>
 			body {background: url(bg_2cwebvn.png);color:#00FF00; font-size:16px;}
@@ -9,28 +9,28 @@
 	</head>	
 	<body> 
 		<font face="Bodoni" size="5" color="red">
-		<center><h1> BANG THEM MON HOC</h1></center>
+		<center><h1> BẢNG THÊM MÔN HỌC</h1></center>
 		<center>
 		<form action="check_user.php" method="post">                    
 			<table align="centre" border="0"  id="table" style="margin-left-"150px"; border-collapse:collapse; width:100%; ">
 
 				<tr>  
 					<td>
-						<strong>Ma so sinh vien</strong><br />
+						<strong>Mã số sinh viên</strong><br />
 						<input name="mssv" value="" type="text">
 					</td>
 				</tr>
 
 				<tr>
 					<td>
-						<strong>Ho va ten</strong><br />
+						<strong>Họ và tên</strong><br />
 						<input name="fullname" value="" type="text">
 					</td>
 				</tr>
                                 
 				<tr>
 					<td>
-						<strong>Mat khau dang nhap</strong><br />
+						<strong>Mật khẩu đăng nhập</strong><br />
 						<input name="password" value="" type="text">
 					</td>
 				</tr>
@@ -43,7 +43,7 @@
 
 			</table>               
 		</form>
-		<p><a href='index.php'>Nhap vao day de quay lai trang chu</a></p>
+		<p><a href='index.php'>Nhấp vào đây để quay lai trang chủ</a></p>
 		</center>
 	</body>
 </html>

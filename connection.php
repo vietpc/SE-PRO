887ccb9043ae8234cef3412fd1b87639035e0099
vietpc <vietpc_56@vnu.edu.vn>
2013-05-29 21:14:56 +0700

    Update and rename connection.php to index.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$host="localhost"; 
-$username="root"; 
-$password=""; 
-$database="userlogin"; 
-mysql_pconnect($host,$username,$password); 
-mysql_query("set names 'utf8'"); 
-mysql_select_db($database);     
-?>
---
+++ index.php
@@ -0,0 +1,56 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<html>
+  <style type='text/css'>
+	body{
+		background: #eee;
+		color: #e00;
+	}
+	a{
+		color:#00e;
+	}
+	a:hover{
+		color: #ee0;
+	}
+	</style>
+	<body>
+		<p align ="center" 
+			style ="margin-left: 10; margin-right: 5;
+					margin-top: 20; margin-bottom: 0">	
+			<strong>Chương Trình Quản Lí Đào Tạo</strong>
+		</p>
+		<p align ="center" 
+			style ="margin-left: 10; margin-right: 5; margin-top: 20;
+					margin-bottom: 0">
+					<strong>Wellcome ! Admin.</strong>
+		</p><br/>
+	    <table align=center border=2 width=500>
+		    <tr>
+				<td>
+					<ul>
+						<li><a href='form_add_user.php'>
+							Tạo Tài Khoản Sinh Viên
+						</li>
+						<li><a href='form_delete_user.php'>
+							Xóa Tài Khoản Sinh Viên
+						</li>
+						<li><a href='form_add_subject.php'>
+							Thêm Môn Học
+						</li>
+						<li><a href='form_delete_subject.php'>
+							Xóa Môn Học
+						</li>
+						<li><a href='list_student.php'>
+							Danh Sach Các Sinh Viên
+						</li>
+						<li><a href='list_subject.php'>
+							Các Môn Học Kỳ Này
+						</li>
+						<li><a href='logout.php'>
+							Thoát
+						</li>
+					</ul>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>

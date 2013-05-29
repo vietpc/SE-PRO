d52c93c0bd7222ab5667f7f98a331685b1dfd5d0
vietpc <vietpc_56@vnu.edu.vn>
2013-05-29 21:00:59 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,26 +1,41 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
 <?php
-  $host="localhost"; // luôn luôn là localhost 
-	$username="root"; // user của mysql 
-	$password=""; // Mysql password 
-	$db_name="test"; // tên database 
-	$tbl_name="member"; // tên table 
-	// kết nối cơ sở dữ liệu 
-	mysql_connect("$host", "$username", "$password")or die("Error connect"); 
-	mysql_select_db("$db_name")or die("cannot select DB");
-	if(isset($_POST['Delete'])){
-		$deluser = $_POST['deluser'];
-		$result =  "DELETE FROM member WHERE username = '$deluser'";
-		$query = mysql_query($result);
-		if($query){
-			header('location:admin.php');
+	require_once("connect.php");
+	$msmh = $_POST['msmh'];
+	if( ! $msmh) {
+		echo "Xin vui lòng nhập mã số môn học cần xóa.";
+		echo "<p><a href='javascript:history.go(-1)'>
+			Nhấp vào đây để quay trở lại</a></p>";
+		echo "<p><a href='index.php'>
+			Nhấp vào đây để quay trở lại trang chủ</a></p>";
+        exit;
+	}else{
+		$sql = "SELECT * FROM mon_hoc WHERE	 ma_mon_hoc = '{$msmh}'";
+		$check = @mysql_query($sql);
+		$result = @mysql_num_rows($check);
+		if( $result <= 0 ){
+			echo "Mã số môn học này không tồn tại";
+			echo "<p><a href='javascript:history.go(-1)'>
+				Nhấp vào đây để quay trở lại</a></p>";
+			echo "<p><a href='index.php'>
+				Nhấp vào đây để quay trở lại trang chủ</a></p>";
+			exit;
+		}else{
+			$query = "DELETE FROM mon_hoc WHERE ma_mon_hoc = '{$msmh}'";
+			$effect = @mysql_query($query);
+			if($effect){
+				echo "Đã xóa thành công";
+                echo "<p><a href='list_subject.php'>
+					Nhấp vào đây để xem danh sách các môn học</a></p>";
+                echo "<p><a href='index.php'>
+					Nhấp vào đây để quay trở lại trang chủ</a></p>";
+			}else{
+				echo "Có lỗi xảy ra trong quá trình thao tác vói CSDL.";
+				echo "<p><a href='javascript:history.go(-1)'>
+					Nhấp vào đây để quay trở lại</a></p>";
+                echo "<p><a href='index.php'>
+					Nhấp vào đây để quay trở lại trang chủ</a></p>";
+                exit;
+			}
 		}
-		else{
-			echo "Chưa Xóa được";
-		}
-	}
-	else{
-		echo "Error connect";
 	}
 ?>
